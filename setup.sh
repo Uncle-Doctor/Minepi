37d4905594ca59b7faee22d0541927b37b8089b4
Uncle-Doctor <kaindl.fabian@web.de>
2017-03-12 09:34:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install xcompmgr libgl1-mesa-dri && sudo apt-get -y install libalut0 libalut-dev && sudo apt-get -y install mesa-utils
